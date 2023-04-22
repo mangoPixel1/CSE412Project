54bc105b0af43ca5bc33db42049feddff39d3782
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2023-04-21 18:33:56 -0700

    User registration complete

--- main.py
+++ main.py
@@ -9,33 +9,69 @@ mydb = mysql.connector.connect(
 )
 
 userLoggedIn = False
+userID = 0 # userID of current user that is logged in
+
 
 print("PhotoShare")
-
-userEmail = ""
-userPassword = ""
 
 while not userLoggedIn:
     mycursor = mydb.cursor()
     mycursor.execute("select userID, email, password from Users")
     myresult = mycursor.fetchall()
-    #for x in myresult:
-        #print(x)
+    for x in myresult:
+        print(x)
 
     userInput = input("Select an option: (l) Login, (r) Register ")
     if userInput == "l":
         print("Log in as an existing user")
         userEmail = input("Enter your email address: ")
         userPassword = input("Enter your password: ")
+        sqlCommand = f"select password from Users where email = \"{userEmail}\""
+        mycursor.execute(sqlCommand)
+
+        myResult = mycursor.fetchone()[0]
+        if userPassword == myResult:
+            print("Login successful")
+            userLoggedIn = True
+            sqlCommand = f"select userID from Users where email = \"{userEmail}\""
+            mycursor.execute(sqlCommand)
+            myResult = mycursor.fetchone()[0]
+            userID = int(myResult)
+        else:
+            print("Invalid password")
+        
+        '''
+        if mycursor.fetchone()[0] is None: # if email is valid
+                myResult = mycursor.fetchone()[0]
+                if userPassword == myResult: # if email is valid and password is valid
+                    print("Login successful")
+                    userLoggedIn = True
+                    sqlCommand = f"select userID from Users where email = \"{userEmail}\""
+                    mycursor.execute(sqlCommand)
+                    myResult = mycursor.fetchone()[0]
+                    userID = int(myResult)
+                else: # if email is valid and password is invalid
+                    print("Invalid password")
+        else: #if email is invalid
+             print("Invalid email")
+        '''
     
     if userInput == "r":
         print("Register as a new user")
         fName = input("Enter your first name: ")
         lName = input("Enter your last name: ")
         emailAddr = input("Enter your email address: ")
-        dob = input("Enter your date of birth (YYYY-MM--DD): ")
+        dob = input("Enter your date of birth (YYYY-MM-DD): ")
+        hometown = input("Enter your hometown: ")
+        gender = input("Enter your gender (Male/Female): ")
+        password = input("Enter a secure password: ")
 
+        sqlCommand = f"insert into Users (fName, lName, email, dateOfBirth, hometown, gender, password) values(\"{fName}\", \"{lName}\", \"{emailAddr}\", \"{dob}\", \"{hometown}\", \"{gender}\", \"{password}\")"
+        mycursor.execute(sqlCommand)
+        mydb.commit()
+        print("User created. Log in to get started.")
 
-
+# Everything past this point is only accessible to logged in users
+print("What do you want to do? Select an option: ")
 
 mydb.close()
